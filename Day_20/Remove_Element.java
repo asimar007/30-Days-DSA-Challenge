ddde6dbd977c0367c02b0e74ee1b80184273715a
Asim Sk <blackhatcode786@gmail.com>
2024-03-09 09:48:50 +0530

    Day-20

---
+++ Day_20/Remove_Element.java
@@ -0,0 +1,19 @@
+public class Remove_Element {
+	public static int Remove(int[] arr, int n, int val) {
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] != val) {
+				arr[count++] = arr[i];
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 3, 2, 2, 3 };
+		int n = arr.length;
+		int val = 3;
+		int ans = Remove(arr, n, val);
+		System.out.println(ans);
+	}
+}
